3afef3fe6f2696f894c4937e6f2e01670c15f60f
ccabreraq <ccabreraq@gmail.com>
2024-04-26 20:56:29 -0500

    v2

--- main.py
+++ main.py
@@ -35,32 +35,44 @@ app.add_middleware(
 
 
 # we could use this as a guide for our chatbot to avoid political conversations
-politics = Route(
-    name="politics - ggggggggggggggggggggggggggggggg",
+funcion1 = Route(
+    name="funcion1|https://flowise-y3q2.onrender.com/api/v1/prediction/28e85d20-87ed-493d-8860-60241c9250e9",
+    utterances=[
+        "enviar un correo",
+        "enviar un sms",
+        "total de facturas",
+        "cantidades de facturas o documentos",
+        "calculadora",
+    ],
+)
+
+# this could be used as an indicator to our chatbot to switch to a more
+# conversational prompt
+funcion2 = Route(
+    name="funcion2|https://flowise-y3q2.onrender.com/api/v1/prediction/2229075d-471b-4a0d-a66a-b06ba1941ee2",
+    utterances=[
+        "verificar en ofac",
+        "verificar una persona",
+        "listas de lavados de activos",
+        "listas restrictivas",
+    ],
+)
+
+no_permitido = Route(
+    name="no_permitido|no_permitido",
     utterances=[
         "cuales son lo mejores politicos",
         "que opinas del presidente petro",
         "de que partido eres",
         "la politica es buena?",
         "quien salvra a este pais",
-    ],
-)
-
-# this could be used as an indicator to our chatbot to switch to a more
-# conversational prompt
-chitchat = Route(
-    name="chitchat",
-    utterances=[
-        "how's the weather today?",
-        "how are things going?",
-        "lovely weather today",
-        "the weather is horrendous",
-        "let's go to the chippy",
+		"te gusta el sexo",
+		""
     ],
 )
 
 # we place both of our decisions together into single list
-routes = [politics, chitchat]
+routes = [funcion1,funcion2,no_permitido]
 
 rl = RouteLayer(encoder=encoder, routes=routes)
 
